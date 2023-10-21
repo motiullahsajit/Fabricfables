5d2924190379ca5249fe9b7456e40217dc4b4a08
motiullahsajit <sajitmotiullah@gmail.com>
2023-10-21 11:06:57 +0600

    env setup and navbar added

---
+++ fabricfables-client/src/customer/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainCarousel from "../../components/HomeCarousel/MainCarousel";
+
+function HomePage() {
+  return (
+    <div>
+      <MainCarousel />
+      <div>other sections</div>
+    </div>
+  );
+}
+
+export default HomePage;
